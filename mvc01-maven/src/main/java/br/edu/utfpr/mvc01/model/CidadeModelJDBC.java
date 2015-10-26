243ab915a25c6e6721163306b7802672daa637cf
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-10-26 16:42:53 -0200

    fixing small issues in code

--- mvc01-maven/src/main/java/br/edu/utfpr/mvc01/model/CidadeModelJDBC.java
+++ mvc01-maven/src/main/java/br/edu/utfpr/mvc01/model/CidadeModelJDBC.java
@@ -1,23 +1,34 @@
 package br.edu.utfpr.mvc01.model;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import br.edu.utfpr.mvc01.model.pojo.Cidade;
 
 public class CidadeModelJDBC {
     public List <Cidade> busca (String nome) {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://abobrinha.com/jpa01", "root", "");
-        PreparedStatment ps = c.prepareStatment(
-                "SELECT * FROM Cidades WHERE nome like ?");
-        ps.setString(1, nome + "%");
-        ResultSet rs = ps.executeQuery();
         List <Cidade> lista = new ArrayList <Cidade> ();
-        while (rs.next()) {
-            Cidade c = new Cidade();
-            c.setNome(rs.getString("nome"));
-            lista.add(c);
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://abobrinha.com/jpa01", "root", "");
+            PreparedStatement ps = conn.prepareStatement(
+                    "SELECT * FROM Cidades WHERE nome like ?");
+            ps.setString(1, nome + "%");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Cidade c = new Cidade();
+                c.setNome(rs.getString("nome"));
+                lista.add(c);
+            }
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Erro na conexão com o banco de dados...");
+            e.printStackTrace();
         }
         return lista;
     }
